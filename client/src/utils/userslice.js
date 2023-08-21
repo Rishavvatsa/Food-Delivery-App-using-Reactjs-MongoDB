bbc6bffaf6994ff7f8f63ccae14ad0b7339b665b
Rishav <vatsarishav99@gmail.com>
2023-08-21 18:32:06 +0530

    commit

--- client/src/utils/userslice.js
+++ client/src/utils/userslice.js
@@ -13,26 +13,29 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     loginRedux: (state, action) => {
-      console.log(action.payload.data);
-
+      
+      
       state._id = action.payload.data._id;
       state.firstName = action.payload.data.firstName;
       state.lastName = action.payload.data.lastName;
       state.email = action.payload.data.email;
       state.image = action.payload.data.image;
-      localStorage.setItem("user", JSON.stringify(state));
+      localStorage.setItem("user",JSON.stringify(state));
+      
     },
     logoutRedux: (state, action) => {
       state._id = "";
       state.firstName = "";
       state.lastName = "";
       state.email = "";
-      state.image = "";
+      state.image=""
       localStorage.removeItem("user");
+
     },
   },
 });
 
-export const { loginRedux, logoutRedux } = userSlice.actions;
+
+export const { loginRedux ,logoutRedux} = userSlice.actions;
 
 export default userSlice.reducer;
